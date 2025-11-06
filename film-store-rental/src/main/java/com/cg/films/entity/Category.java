cd2f153686a32aa257af4fe87c324244fe5d174a
mufimaimoon <mufaddal.maimoon@capgemini.com>
2025-11-06 15:48:59 +0530

    Add files via upload

---
+++ film-store-rental/src/main/java/com/cg/films/entity/Category.java
@@ -0,0 +1,67 @@
+package com.cg.films.entity;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+
+    @OneToMany(mappedBy = "category")
+    @JsonIgnore
+    private List<FilmCategory> filmCategories;
+
+    @Transient
+    public List<Film> getFilms() {
+        return filmCategories.stream()
+                .map(FilmCategory::getFilm)
+                .toList();
+    }
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Timestamp getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(Timestamp lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	public List<FilmCategory> getFilmCategories() {
+		return filmCategories;
+	}
+
+	public void setFilmCategories(List<FilmCategory> filmCategories) {
+		this.filmCategories = filmCategories;
+	}
+
+}
